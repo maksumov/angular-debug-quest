91d5729b615805caa96424376d7b7684868d7f19
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2025-02-09 17:13:52 +0100

    feat: add config service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,24 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  ApplicationConfig,
+  inject,
+  provideAppInitializer,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { firstValueFrom } from 'rxjs';
+import { ConfigService } from './shared/services/config.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()],
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    provideAppInitializer(async () => {
+      return async () => {
+        const config = await firstValueFrom(inject(ConfigService).getConfig());
+        inject(ConfigService).config.set(config);
+      };
+    }),
+  ],
 };
